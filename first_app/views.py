82886bdb8cb13be36c3b4bb5e67c007f8799e7e0
Shohagh019 <shohaghmunna@gmail.com>
2024-11-10 12:10:54 +0600

    delete item from frontend added

--- first_app/views.py
+++ first_app/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from . import models
 
 def home(request):
     student = models.Student.objects.all()
     return render(request, 'first_app/home.html', {'data': student})
+
+def delete_item(request, roll):
+    std = models.Student.objects.get(pk=roll).delete()
+    return redirect('homepage')
